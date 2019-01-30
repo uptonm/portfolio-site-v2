1753b9dcbc0b17140cc4e4337764a874c361547f
Mike Upton <uptonm@wit.edu>
2019-01-30 16:07:27 -0500

    Fixing a few things before deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  routeToProfile(where: string) {
+    switch (where) {
+      case "github":
+        window.location.href = "https://github.com/uptonm";
+        break;
+      case "linkedin":
+        window.location.href = "https://linkedin.com/in/uptonm";
+        break;
+      case "twitter":
+        window.location.href = "https://twitter.com/mikeupton34";
+        break;
+      case "codepen":
+        window.location.href = "https://codepen.io/uptonm";
+        break;
+      default:
+        return;
+    }
+  }
+}
--- src/app/repo.service.ts
+++ src/app/repo.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class RepoService implements OnInit {
   constructor(private http: HttpClient) {}
@@ -10,6 +10,6 @@ export class RepoService implements OnInit {
   ngOnInit(): void {}
 
   getRepos() {
-    return this.http.get('http://localhost:8000/api/repos');
+    return this.http.get("https://uptonm-gh-webhooks.herokuapp.com/api/repos");
   }
 }
