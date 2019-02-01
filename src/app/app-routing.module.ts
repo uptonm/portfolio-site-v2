59845d134a8b89d0a08964446657242fa717862d
Michael Upton <uptonm@wit.edu>
2019-02-01 09:32:02 -0500

    added about page to site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RepoListComponent } from './repo-list/repo-list.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'repos', component: RepoListComponent }
+  { path: 'repos', component: RepoListComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
